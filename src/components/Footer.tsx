0372613666390580bef32999978c334d9c7f6324
SivThean <sivtheanv@gmail.com>
2025-06-13 17:24:12 +0700

    Updated rupp scholarship portal

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Mail, Phone, MapPin, Facebook, Twitter, Instagram, Linkedin, MessageCircle, } from 'lucide-react';
+import { Mail, Phone, MapPin, Facebook, Twitter, Instagram, Linkedin, } from 'lucide-react';
 import rupplogo from '../assets/rupplogo-removebg-preview.png';
 
 // Telegram SVG icon as a React component
